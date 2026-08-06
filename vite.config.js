58c9e64854f5f5d56beba2284d98314b40ef03d5
HAFIDZUL AMIN <hafidzulamin2026@gmail.com>
2026-08-06 13:01:28 +0800

    Resolve vite config merge conflict

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/myrizab/',
-=======
-  base: './',
->>>>>>> 03c3d34d23622c3abac87346f0fd6ab22ea8adb3
   server: {
     host: true,
     port: 5173
